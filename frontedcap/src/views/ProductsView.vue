7b9bd9c2331eeca1e79c45fc484e6f579c79a2ed
UnathiGcetywa80 <146818955+UnathiGcetywa80@users.noreply.github.com>
2024-03-21 03:52:29 +0200

    Update ProductsView.vue

--- frontedcap/src/views/ProductsView.vue
+++ frontedcap/src/views/ProductsView.vue
@@ -5,16 +5,21 @@
       <button @click="sortBy('asc')">SortByCategory</button>
     </div>
 
-    <div class="card" v-for="products in product" :key="products.id" style="width: 18rem;">
-      <img :src="products.Image" class="card-img-top" :alt="products.Image">
-      <div class="card-body">
-        <h5 class="card-title">{{ products.productNam }}</h5>
-        <p class="card-text">{{ products.decription }}</p>
-        <router-link class="btn btn-primary" :to="{ name: 'product', params: { id: products.product_id } }">
-          View More
-        </router-link>
+    <div class="row d-flex justify-content-center">
+
+      <div class="card m-2" v-for="products in product" :key="products.id" style="width: 18rem;">
+        <img :src="products.Image" class="card-img-top p-2" :alt="products.Image">
+        <div class="card-body">
+          <h5 class="card-title">{{ products.product_name }}</h5>
+          <p class="card-text">{{ products.decription }}</p>
+          <router-link class="btn btn-primary" :to="{ name: 'product', params: { id: products.product_id } }">
+            View More
+          </router-link>
+        </div>
       </div>
     </div>
+
+
   </div>
 </template>
 <script>
@@ -43,7 +48,6 @@ export default {
     }
   },
   mounted() {
-    console.log(this.$store.state.products);
     this.$store.dispatch('fetchProducts')
   }
 }
