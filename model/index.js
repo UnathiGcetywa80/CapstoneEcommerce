8a9f1ba65e17b9df856075ee2fa19d4d6b372209
UnathiGcetywa80 <146818955+UnathiGcetywa80@users.noreply.github.com>
2024-03-21 02:35:24 +0200

    Update index.js

--- model/index.js
+++ model/index.js
@@ -1,11 +1,11 @@
 import {Users} from '../model/users.js';
 import {Products} from '../model/products.js';
-
+import { Cart } from './Cart.js';
 let users = new Users()
 let products = new Products()
-
+let cart = new Cart()
 export{
     users,
-    products
+    products,
+    cart
 }
-//index.js
